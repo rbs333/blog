592b634d0ee63256085c9b002c04069b11624189
Robert Shelton <robes98@vt.edu>
2021-02-05 10:49:58 -0500

    angular update

--- src/app/archive-item/archive-item.component.spec.ts
+++ src/app/archive-item/archive-item.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ArchiveItemComponent } from './archive-item.component';
 
@@ -6,7 +6,7 @@ describe('ArchiveItemComponent', () => {
   let component: ArchiveItemComponent;
   let fixture: ComponentFixture<ArchiveItemComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ArchiveItemComponent ]
     })
--- src/app/checkout-element/checkout-element.component.spec.ts
+++ src/app/checkout-element/checkout-element.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CheckoutElementComponent } from './checkout-element.component';
 
@@ -6,7 +6,7 @@ describe('CheckoutElementComponent', () => {
   let component: CheckoutElementComponent;
   let fixture: ComponentFixture<CheckoutElementComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CheckoutElementComponent ]
     })
