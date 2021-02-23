4bf02b2eae289b6321f5654432bb8f9200d7d49e
Robert Shelton <robes98@vt.edu>
2021-02-23 08:56:36 -0500

    update product wording

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -14,14 +14,14 @@ export class ShopComponent implements OnInit {
     {
       "name": "Bobby's Famous Pancake Mix",
       "amount": "1500",
-      "description": "A pancake tradition forged over years of pancakes Saturdays. An order ships with 64oz of mix!",
+      "description": "A tradition forged over years of pancakes Saturdays. An order ships with 32oz of mix!",
       "image": "https://rbs-storage.s3.amazonaws.com/website+content/pancake_pic.jpg",
       "isShirt": false
     },
     {
       "name": "Bobby's Famous Pancake Mix",
       "amount": "500",
-      "description": "NO SHIPPING EDITION - Enjoy A pancake tradition forged over years of pancakes Saturdays. An order ships with 64oz of mix! Here's the rub: To put pancake mix in a box and ship it to you, my dear fellow pancake lover, would cost me around $12. So instead of inflating the price for shipping, buy it for cheap and the next time I play a show around your area I'll bring it to you in person! If I don't pay for shipping - you don't pay for shipping!! Everybody wins!!!",
+      "description": "NO SHIPPING EDITION - Enjoy A tradition forged over years of pancakes Saturdays. An order ships with 32oz of mix! Here's the rub: To put pancake mix in a box and ship it to you, my dear fellow pancake lover, would cost me around $12. So instead of inflating the price for shipping, buy it for cheap and the next time I play a show around your area I'll bring it to you in person! If I don't pay for shipping - you don't pay for shipping!! Everybody wins!!!",
       "image": "https://rbs-storage.s3.amazonaws.com/website+content/pancake_pic.jpg",
       "isShirt": false
     }
