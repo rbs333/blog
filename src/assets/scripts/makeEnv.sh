7c515a8ed8d2c36bf6de44f1b6ff15386663a641
Robert Shelton <robes98@vt.edu>
2021-03-02 20:21:54 -0500

    change to shell script

--- src/assets/scripts/makeEnv.sh
+++ src/assets/scripts/makeEnv.sh
@@ -1,8 +1,9 @@
 
-mkdir ./src/environment
+mkdir ./src/environments
 echo "export const environment = {
     production: '$1',
     PUBLIC_STRIPE_KEY: '$2',
     CHARGE_URL: '$3',
     API_URL: '$4'
-};" > ./src/environment/environment.prod.ts
+};" > ./src/environments/environment.ts
+cp ./src/environments/environment.ts ./src/environments/environment.prod.ts
