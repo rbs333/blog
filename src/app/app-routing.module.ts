4b24ced2b3a91625741f109148b5a064c4936ebc
Robert Shelton <robes98@vt.edu>
2021-04-06 08:33:08 -0400

    add pancake page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { ManifestoComponent } from './manifesto/manifesto.component';
 import { ArchiveItemComponent } from './archive-item/archive-item.component';
 import { ShopComponent } from './shop/shop.component';
 import { HomeComponent } from './home/home.component';
+import { PancakeComponent } from './pancake/pancake.component';
 
 
 const routes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'manifesto', component: ManifestoComponent },
+    { path: 'pancakes', component: PancakeComponent },
     { path: 'archive', component: ArchiveComponent},
     { path: "archive/:title", component: ArchiveItemComponent},
     { path: "shop", component: ShopComponent}
