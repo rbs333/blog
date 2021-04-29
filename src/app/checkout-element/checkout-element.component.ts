bd6d79540813f4e3267f311200d61929ea7b009d
Robert Shelton <robes98@vt.edu>
2021-04-28 22:11:52 -0400

    add venmo toggle

--- src/app/checkout-element/checkout-element.component.ts
+++ src/app/checkout-element/checkout-element.component.ts
@@ -18,7 +18,8 @@ export class CheckoutElementComponent implements OnInit {
   stripe;
   card;
   cardErrors;
-  is_venmo = false;
+  mobile = false;
+  isVenmo = false;
   loading = false;
   confirmation;
 
@@ -35,8 +36,8 @@ export class CheckoutElementComponent implements OnInit {
                                   Validators.maxLength(2)]),
       zip: new FormControl("", [Validators.required,
                                 Validators.minLength(5)]),
-      shirt_size: new FormControl("", Validators.minLength(1)),
-      venmo_handle: new FormControl(false, Validators.required),
+      shirtSize: new FormControl("", Validators.minLength(1)),
+      venmoHandle: new FormControl("", Validators.required),
     })
   }
 
@@ -58,45 +59,35 @@ export class CheckoutElementComponent implements OnInit {
   }
 
   get name() { return this.formGroup.get('name'); }
-  get venmo_handle() { return this.formGroup.get('venmo_handle'); }
+  get venmoHandle() { return this.formGroup.get('venmoHandle'); }
   get email() { return this.formGroup.get('email'); }
   get address() { return this.formGroup.get('address'); }
   get city() { return this.formGroup.get('city'); }
   get state() { return this.formGroup.get('state'); }
   get zip() { return this.formGroup.get('zip'); }
-  get shirt_size() { return this.formGroup.get('shirt_size'); }
+  get shirtSize() { return this.formGroup.get('shirtSize'); }
 
   togglePayment = () => {
-    this.is_venmo = !this.is_venmo;
+    this.isVenmo = !this.isVenmo;
   };
 
   async handleForm(e, formData) {
     e.preventDefault();
 
-    const { source, error } = await this.stripe.createSource(this.card);
+    if (this.isVenmo || this.mobile) {
+      // if venmo or mobile don't allow credit card
+      formData.amount = this.product.amount;
+      formData.date = Date();
+      formData.id = formData.email;
 
-    if (error) {
-      this.cardErrors = error.message;
-    }  else {
-      this.loading = true;
-      this.stripeService.createCharge(
-        {
-          amount: this.product.amount, 
-          currency: 'USD',
-          stripeToken: source.id
-        }
-      ).subscribe(() => {
-        formData.amount = this.product.amount;
-        formData.date = Date();
-        formData.id = formData.email;
-
-        this.stripeService.postAddressInfo(formData)
+      // post form with venmo handle for payment
+      this.stripeService.postAddressInfo(formData)
           .subscribe(
             (res) => {
               this.confirmation = res;
               this.loading = false;
               this.activeComponent.dismiss();
-              alert("Thank you! Payment Received.")
+              alert("Thank you! You should receive a venmo request soon :)")
           },
             error => {
               this.loading = false;
@@ -104,13 +95,51 @@ export class CheckoutElementComponent implements OnInit {
               alert("Something went wrong :(")
             }
           );
-      },
-      error => {
-        this.activeComponent.dismiss();
-        alert("Something went wrong :(");
-        this.loading = false;
+    } else {
+      // run credit card process
+      const { source, error } = await this.stripe.createSource(this.card);
+
+      if (error) {
+        this.cardErrors = error.message;
+      }  else {
+        this.loading = true;
+
+        // create charge
+        this.stripeService.createCharge(
+          {
+            amount: this.product.amount, 
+            currency: 'USD',
+            stripeToken: source.id
+          }
+        ).subscribe(() => {
+          // add amounts to form data
+          formData.amount = this.product.amount;
+          formData.date = Date();
+          formData.id = formData.email;
+
+          // if successful post information to DB
+          this.stripeService.postAddressInfo(formData)
+            .subscribe(
+              (res) => {
+                this.confirmation = res;
+                this.loading = false;
+                this.activeComponent.dismiss();
+                alert("Thank you! Payment Received.")
+            },
+              error => {
+                this.loading = false;
+                this.activeComponent.dismiss();
+                alert("Something went wrong :(")
+              }
+            );
+        },
+        error => {
+          this.activeComponent.dismiss();
+          alert("Something went wrong :(");
+          this.loading = false;
+        }
+        )
       }
-      )
     }
   }
 }
