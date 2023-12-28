c629e4e55507af4cc8f1255f656ab8c5ed00c6c3
rbs333 <robes98@vt.edu>
2023-12-28 13:24:38 -0500

    remove shop

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  API_URL: "https://aq9j0pujd0.execute-api.us-east-1.amazonaws.com/beta",
+  PUBLIC_STRIPE_KEY: "blah",
+  CHARGE_URL: "double_blah",
+};
